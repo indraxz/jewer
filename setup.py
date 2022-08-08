c5e1bb159f25312849b993de01c5f26ec79ff82c
indraxz <indraskd80@gmail.com>
2022-08-08 22:01:13 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "jewer.py",
+    version = "0.1",
+    py_modules = ['brekele'],
+    url = "https://github.com/indraxz/jewer.git",
+    author = "indraxz",
+    install_requires = ["pyngrok"]
+)
