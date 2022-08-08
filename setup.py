767d2380a04c2c4c80e11dacc8e80a5d4f41adee
indraxz <indraskd80@gmail.com>
2022-08-08 22:08:10 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "jewer.py",
     version = "0.1",
-    py_modules = ['brekele'],
+    py_modules = ['jewer'],
     url = "https://github.com/indraxz/jewer.git",
     author = "indraxz",
     install_requires = ["pyngrok"]
