bf438797b9bffcb7ed50bf4b1e545354577a5db1
indraxz <indraskd80@gmail.com>
2022-09-20 13:27:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = "jewer.py, abang.py",
-    version = "0.1",
-    py_modules = ['abang, abang'],
-    url = "https://github.com/indraxz/jewer.git",
-    author = "indraxz",
-    install_requires = ["pyngrok"]
-)
-
-setup(
-    name = "jewer.py",
+    name = "abang.py",
     version = "0.1",
     py_modules = ['abang'],
     url = "https://github.com/indraxz/jewer.git",
