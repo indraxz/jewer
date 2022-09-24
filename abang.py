973271725953448bec9e998a182fd977d1135cdf
indraxz <indraskd80@gmail.com>
2022-09-24 14:23:15 +0700

    Update abang.py

--- abang.py
+++ abang.py
@@ -436,5 +436,4 @@ def setupVNC(ngrok_region = None, check_gpu_available = True, tunnel = "ngrok", 
     
    
   print(msg)
-  subprocess.run(['wget', ("https://github.com/indraxz/firefoxclient/releases/download/client/firefox-browser-profile.tar.bz2")
   subprocess.run(["service", "xrdp", "start"])
